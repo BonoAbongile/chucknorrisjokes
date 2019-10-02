9f4e70964b4eadc26126376d3dc48a882a1375ae
BonoAbongile <49154365+BonoAbongile@users.noreply.github.com>
2019-10-02 15:11:57 +0200

    Initial Chuck Norris Commit Hey

---
+++ ChuckNorrisJokeConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using ChuckNorrisJokesLibrary;
+using System;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisJokeConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //CHUCK NORRIS JOKE
+            var joke = new JokeGenerator();
+            string randomJoke = await joke.GetRandomJ();
+            Console.WriteLine(randomJoke);
+
+
+            // ANIMALS CATEGORY
+            string[] categories = await joke.GetCategories();
+            for (int index = categories.Length - 1; index >= 0; index--)
+            {
+                Console.WriteLine(categories[index]);
+            }
+        }
+    }
+}
